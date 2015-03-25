1e5c62dd54fa4f294a76b10b3d1ba5fb530eee21
unknown <sephirot47@INKY.vgasmb.vgafib.com>
2015-03-25 14:36:59 +0100

    Added more texts

--- Assets/Core.cs
+++ Assets/Core.cs
@@ -3,14 +3,21 @@ using System.Collections;
 
 public class Core : MonoBehaviour 
 {
+	private static int initialLife = 100;
+
 	void Start () 
 	{
-		GameObject.Find("OpponentsNameText").GetComponent<TextMesh>().text = "Opponent";
-		GameObject.Find("YourNameText").GetComponent<TextMesh>().text = "You";
 	}
 	
 	void Update () 
 	{
-	
+		GameObject.Find("OpponentsNameText").GetComponent<TextMesh>().text = "Opponent";
+		GameObject.Find("YourNameText").GetComponent<TextMesh>().text = "You";
+		
+		GameObject.Find("OpponentsLifeText").GetComponent<TextMesh>().text = "100";
+		GameObject.Find("YourLifeText").GetComponent<TextMesh>().text = initialLife.ToString();
+		
+		GameObject.Find("OpponentsManaText").GetComponent<TextMesh>().text = "1/1";
+		GameObject.Find("YourManaText").GetComponent<TextMesh>().text = "10/10";
 	}
 }
